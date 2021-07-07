69e1f94c0de6d535e83e95023faa25fac9fc6830
~ Matziar ~ <=>
2021-07-07 09:20:15 -0600

    I've added a little more to the battle scene

--- project_template/Kevin_DPM/game/scene.py
+++ project_template/Kevin_DPM/game/scene.py
@@ -13,18 +13,22 @@ class Scene:
         for character in characters:
             character.draw()
 
-    def battle_display(self, player, professor):
+    def battle_display(self, player, professor, left, bottom):
+        player.battleSprite.center_x = player.battleSprite.screen_pos_x + left
+        player.battleSprite.center_y = player.battleSprite.screen_pos_y + bottom
         player.battleSprite.draw()
         
+        professor.battleSprite.center_x = professor.battleSprite.screen_pos_x + left
+        professor.battleSprite.center_y = professor.battleSprite.screen_pos_y + bottom
         professor.battleSprite.draw()
-        self.draw_bars(player, professor)
+        self.draw_bars(player, professor, left, bottom)
 
-    def draw_bars(self, player, professor):
+    def draw_bars(self, player, professor, left, bottom):
         # Player's
         bar_max = constants.SCREEN_WIDTH/2 
         bar_current = (player.stanima/player.stanima_max)*(constants.SCREEN_WIDTH/2)
-        arcade.draw_rectangle_filled(player.battleSprite.center_x, constants.SCREEN_HEIGHT - 25, bar_max, 25, arcade.csscolor.WHITE)
-        arcade.draw_rectangle_filled(player.battleSprite.center_x, constants.SCREEN_HEIGHT - 25, bar_current, 25, arcade.csscolor.LIGHT_STEEL_BLUE)
+        arcade.draw_xywh_rectangle_filled(left + 10, constants.SCREEN_HEIGHT - 35 + bottom, bar_max, 25, arcade.csscolor.WHITE)
+        arcade.draw_xywh_rectangle_filled(left + 10, constants.SCREEN_HEIGHT - 35 + bottom, bar_current, 25, arcade.csscolor.LIGHT_STEEL_BLUE)
 
         # Professor's
-        arcade.draw_text(f"Task: {professor.task_health}", professor.battleSprite.center_x - 48, constants.TEXT_BOX_HEIGHT - 20, arcade.csscolor.WHITE, 48)
+        arcade.draw_text(f"Task: {professor.task_health}", (professor.battleSprite.center_x - 48), (constants.TEXT_BOX_HEIGHT - 10) + bottom, arcade.csscolor.WHITE, 48)
--- project_template/Kevin_DPM/game/text.py
+++ project_template/Kevin_DPM/game/text.py
@@ -14,10 +14,14 @@ class Text():
         
     def print_text(self, text, show_has_finished = False):
         # print(text)
+        text_list = self.group_by_words(text)
+
         if self.display_text == "":
             self.display_text_count = 0
         if self.display_text.replace("\n", "") != text:
             self.display_text += text[self.display_text_count]
+            # self.display_text += text_list[self.display_text_count]
+            # if len(text_list[self.display_text_count]) + len(self.display_text) % 35 >= 0 and len(self.display_text) < 35 - len(text_list[self.display_text_count]):
             if self.display_text_count % 35 == 0 and self.display_text_count > 0:
                 self.display_text += "\n"
             self.display_text_count += 1
@@ -33,6 +37,9 @@ class Text():
     def battle_prof_text(self, prof_name, prof_move, player_move, show_has_finished = False):
         text = f"Professor {prof_name} used {prof_move}. You used {player_move}."
         return self.print_text(text,show_has_finished)
+
+    def group_by_words(self, text):
+        return text.split()
 
     def clear_text(self):
         self.display_text = ""
